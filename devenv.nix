8554e7d7b86d6dfb5bdd2219950fe84c9d55d593
Xandor Schiefer <me@xandor.co.za>
2024-04-27 14:39:44 +0200

    feat: init yarn

--- devenv.nix
+++ devenv.nix
@@ -1,37 +1,47 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  languages.javascript.enable = true;
+  languages.javascript.corepack.enable = true;
+  languages.typescript.enable = true;
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  enterShell = let
+    # Adapted from https://github.com/cachix/devenv/blob/main/src/modules/languages/javascript.nix
+    initYarnScript = pkgs.writeShellScript "init-yarn.sh" ''
+      function _devenv-yarn-install()
+      {
+        # Avoid running "yarn install" for every shell.
+        # Only run it when the "yarn.lock" file or nodejs version has changed.
+        # We do this by storing the nodejs version and a hash of "yarn.lock" in node_modules.
+        local ACTUAL_YARN_CHECKSUM="$(yarn --version):$(${pkgs.nix}/bin/nix-hash --type sha256 yarn.lock)"
+        local YARN_CHECKSUM_FILE="${config.env.DEVENV_STATE}/yarn.lock.checksum"
+        if [ -f "$YARN_CHECKSUM_FILE" ]
+          then
+            read -r EXPECTED_YARN_CHECKSUM < "$YARN_CHECKSUM_FILE"
+          else
+            EXPECTED_YARN_CHECKSUM=""
+        fi
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+        if [ "$ACTUAL_YARN_CHECKSUM" != "$EXPECTED_YARN_CHECKSUM" ]
+        then
+          if yarn install
+          then
+            [ ! -d "${config.env.DEVENV_STATE}" ] && mkdir -p "${config.env.DEVENV_STATE}"
+            echo "$ACTUAL_YARN_CHECKSUM" > "$YARN_CHECKSUM_FILE"
+          else
+            echo "Install failed. Run 'yarn install' manually."
+          fi
+        fi
+      }
 
-  enterShell = ''
-    hello
-    git --version
+      if [ ! -f package.json ]
+      then
+        echo "No package.json found. Run 'yarn init' to create one." >&2
+      else
+        _devenv-yarn-install
+      fi
+    '';
+  in ''
+    "${initYarnScript}"
   '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep "2.42.0"
-  '';
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
